cd641589a34685d070ef303504937399e4f3b74d
Ivaylo Goranovski <ivaylo.goranovski@gmail.com>
2022-02-17 14:29:43 +0200

    first commit

---
+++ src/com/estafet/learning/US002/Main.java
@@ -0,0 +1,22 @@
+package com.estafet.learning.US002;
+
+import com.estafet.learning.US003.Task3;
+
+public class Main {
+    public static void main(String[] args) {
+        byte b = 127;
+        System.out.println(b);
+
+        boolean t  = true;
+        System.out.println(t);
+
+        char capitalC = 'C';
+        System.out.println(capitalC);
+
+        String sampleString = "lorem ipsum";
+        System.out.println(sampleString);
+
+        Task3 task3 = new Task3();
+        task3.task3();
+    }
+}
---
+++ src/com/estafet/learning/US003/Task3.java
@@ -0,0 +1,26 @@
+package com.estafet.learning.US003;
+
+public class Task3 {
+    public void task3(){
+        byte b = 100;
+        short s = 10000;
+        int i = 100000;
+        long l = 321312312;
+        float f = 3213.32f;
+        double d = 23123;
+        char capitalC = 'C';
+        boolean result = true;
+        Byte.toString(b);
+        Short.toString(s);
+        Float.toString(f);
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(capitalC);
+        System.out.println(result);
+    }
+
+}
