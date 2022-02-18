de787dbc4c7fae0291bf4e97d2d9a218584dc1e2
Ivaylo Goranovski <ivaylo.goranovski@gmail.com>
2022-02-18 09:51:30 +0200

    Fibonacii task

--- src/com/estafet/learning/US005/Task5.java
+++ src/com/estafet/learning/US005/Task5.java
@@ -7,6 +7,7 @@ public class Task5 {
         checkNumberIsEvenOrOdd();
         Fibonacii();
     }
+    public static int number;
 
     public static void checkNumberIsEvenOrOdd() {
         Scanner reader = new Scanner(System.in);
@@ -22,22 +23,26 @@ public class Task5 {
 
     public static void Fibonacii() {
         Scanner reader = new Scanner(System.in);
-        System.out.print("Enter a number to view Fibonacci series: ");
-        int count = reader.nextInt();
-/*
-        while(count < 0){
-            System.out.println("Please enter a number that is greater than 0");
-        }
-*/
+        System.out.format("Enter a number to view Fibonacci series: %n");
+
+        do {
+            System.out.format("Please enter a positive number!");
+            while (!reader.hasNextInt()) {
+                System.out.println("That's not a number!");
+                System.out.format("Enter a number to view Fibonacci series: %n");
+                reader.next(); // this is important!
+            }
+            number = reader.nextInt();
+        } while (number <= 0);
 
         int n1 = 0, n2 = 1, n3, i;
         System.out.print(n1 + " " + n2);
 
-        for (i = 2; i < count; ++i) {
+        for (i = 2; i < number; ++i) {
             n3 = n1 + n2;
             System.out.print(" " + n3);
             n1 = n2;
             n2 = n3;
         }
     }
-}
+    }
