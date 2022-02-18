cefe9144a2555faf3a60f803df07cc79c870f99e
Ivaylo Goranovski <ivaylo.goranovski@gmail.com>
2022-02-18 12:14:41 +0200

    add two numbers

--- src/com/estafet/learning/US005/Task5.java
+++ src/com/estafet/learning/US005/Task5.java
@@ -4,9 +4,13 @@ import java.util.Scanner;
 
 public class Task5 {
     public static void main(String[] args) {
-        checkNumberIsEvenOrOdd();
+/*        checkNumberIsEvenOrOdd();
         Fibonacii();
+        Factorial();
+        FactorialOfOddNumbers();*/
+        addTwoNumbers();
     }
+
     public static int number;
 
     public static void checkNumberIsEvenOrOdd() {
@@ -30,7 +34,7 @@ public class Task5 {
             while (!reader.hasNextInt()) {
                 System.out.println("That's not a number!");
                 System.out.format("Enter a number to view Fibonacci series: %n");
-                reader.next(); // this is important!
+                reader.next();
             }
             number = reader.nextInt();
         } while (number <= 0);
@@ -45,4 +49,54 @@ public class Task5 {
             n2 = n3;
         }
     }
+
+    public static void Factorial() {
+        Scanner reader = new Scanner(System.in);
+        System.out.format("Please enter a positive number to view Factorial of it: %n");
+
+        do {
+            while (!reader.hasNextInt()) {
+                System.out.println("That's not a number!");
+                System.out.format("Enter a positive number to view Factorial of it: %n");
+                reader.next();
+            }
+            number = reader.nextInt();
+        } while (number <= 0);
+
+        long i, fact = 1;
+        for (i = 1; i <= number; i++) {
+            fact = fact * i;
+        }
+        System.out.println("Factorial of " + number + " is: " + fact);
     }
+
+    public static void FactorialOfOddNumbers() {
+        Scanner reader = new Scanner(System.in);
+        System.out.format("Please enter a positive and odd number to view Factorial of it: %n");
+        number = reader.nextInt();
+
+        if (number > 0 && number % 2 != 0) {
+            long i, fact = 1;
+            for (i = 1; i <= number; i++) {
+                fact = fact * i;
+            }
+            System.out.println("Factorial of " + number + " is: " + fact);
+        } else {
+            System.out.format("Please enter a positive and odd number to view Factorial of it: %n");
+        }
+    }
+
+    public static void addTwoNumbers() {
+        Scanner reader = new Scanner(System.in);
+        do {
+            System.out.format("Please enter two numbers to calculate the sum of them: %n");
+            int numberOne = reader.nextInt();
+            int numberTwo = reader.nextInt();
+            int sum = numberOne + numberTwo;
+            System.out.format("The sum of these numbers " + numberOne + " and " + numberTwo + " is : " + sum + "%n");
+
+            System.out.format("Would you like to do this operation again? ");
+        }
+        while (!reader.next().equals("no"));
+    }
+}
