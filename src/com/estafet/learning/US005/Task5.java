77c13c2c75184e5360d8fc0f8fc83ca7e4951835
Ivaylo Goranovski <ivaylo.goranovski@gmail.com>
2022-02-18 16:16:01 +0200

    uncomment

--- src/com/estafet/learning/US005/Task5.java
+++ src/com/estafet/learning/US005/Task5.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class Task5 {
     public static void main(String[] args) {
-        /*checkNumberIsEvenOrOdd();
+        checkNumberIsEvenOrOdd();
         Fibonacii();
         Factorial();
         FactorialOfOddNumbers();
-        addTwoNumbers();*/
+        addTwoNumbers();
         numberOfDaysInMonth();
     }
 
