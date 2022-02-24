2a0ee4c7378b5fca58637cd4a6eb3e4aa6c7d202
Ivaylo Goranovski <ivaylo.goranovski@gmail.com>
2022-02-24 13:36:51 +0200

    first attempt

--- src/com/estafet/learning/US007/Task7.java
+++ src/com/estafet/learning/US007/Task7.java
@@ -9,8 +9,8 @@ import java.util.stream.IntStream;
 public class Task7 {
     public static void main(String[] args) {
         intArray();
-        stringArray();
         swapArrayElements();
+        stringArray();
         getAverageOfArray();
         copyElementsOfArray();
         getMinAndMaxOfArray();
@@ -29,7 +29,6 @@ public class Task7 {
         };
 
         System.out.println("\n------------------------");
-        System.out.println("Array length is: " + intArr.length);
         int sum = 0;
         for (int[] ints : intArr) {
             for (int anInt : ints) {
@@ -41,10 +40,13 @@ public class Task7 {
         int firstElement = intArr[0][0];
         int[] lastRow = intArr[intArr.length - 1];
         int lastElement = lastRow[lastRow.length - 1];
+        System.out.print("\nRows length is: " + intArr.length);
+        System.out.print("\nColumns length is: " + intArr[0].length);
         System.out.print("\nFirst element of array is : " + firstElement);
         System.out.print("\nValue of thirteen element of array is : " + intArr[1][2]);
         System.out.print("\nLast element of array is : " + lastElement);
         System.out.print("\nSum of array elements is : " + sum);
+        System.out.print("\n------------------------");
     }
 
     private static void swapArrayElements() {
@@ -53,14 +55,14 @@ public class Task7 {
         intArr[1][0] = 2;
         int a = intArr[0][0];
         int b = intArr[1][0];
-        System.out.format("Value of first element is : " + a + "%n");
-        System.out.format("Value of second element is : " + b + "%n");
+        System.out.print("\nValue of first element is : " + a);
+        System.out.print("\nValue of second element is : " + b);
         a ^= b;
         b ^= a;
         a ^= b;
-        System.out.format("Swapped value of first element is : " + a + "%n");
-        System.out.format("Swapped value of second element is : " + b + "%n");
-        System.out.println("\n------------------------");
+        System.out.print("\nSwapped value of first element is : " + a);
+        System.out.print("\nSwapped value of second element is : " + b);
+        System.out.print("\n------------------------");
     }
 
     private static void stringArray() {
@@ -69,12 +71,9 @@ public class Task7 {
                 {"eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty"}
         };
 
-        System.out.println("\n------------------------");
-        System.out.println("Array length is: " + stringArr.length);
-
         for (String[] strings : stringArr) {
             for (String string : strings) {
-                System.out.print(string + " ");
+                System.out.print("\nPrinted array elements are : "+string);
             }
         }
 
@@ -100,11 +99,11 @@ public class Task7 {
             }
         }
 
-        System.out.println("\nGet Average of Array:");
-        System.out.format("The total is: " + total + "%n");
+        System.out.print("\nGet Average of Array");
+        System.out.print("\nThe total is: " + total + "%n");
         double average = total / intArr.length;
         System.out.format("The average is: " + average + "%n");
-        System.out.println("\n------------------------");
+        System.out.print("\n------------------------");
     }
 
     private static void copyElementsOfArray() {
@@ -112,7 +111,7 @@ public class Task7 {
         int[] b;
         b = Arrays.copyOf(a, 3);
 
-        System.out.format("Elements of Array a are: %n");
+        System.out.print("\nElements of Array a are: %n");
         for (int j : a) System.out.print(j + " ");
 
         System.out.format("%nElements of Array b are:  %n");
@@ -121,33 +120,57 @@ public class Task7 {
     }
 
     private static void getMinAndMaxOfArray() {
-        int[] a = {13, 43243, 8, 321321312, 43423, -1};
-        Arrays.sort(a);
-        System.out.print("\nMin value of Array is : " + a[0]);
-        System.out.print("\nMax value of Array is : " + a[a.length - 1]);
+        int[][] a = {
+                {13, 43243, 8, 321321312, 43423, -1},
+                {0, 443, 8, 54551, 43423, -10}
+        };
+
+        System.out.print("\nPrinted array is : "+Arrays.deepToString(a));
+        System.out.print("\nMin value of Array is : " + getMinValue(a));
+        System.out.print("\nMax value of Array is : " + getMaxValue(a));
+    }
+    public static int getMaxValue(int[][] numbers) {
+        int maxValue = numbers[0][0];
+        for (int[] number : numbers) {
+            for (int j : number) {
+                if (j > maxValue) {
+                    maxValue = j;
+                }
+            }
+        }
+        return maxValue;
     }
 
-    public static int[] addElement(int n, int[] arr, int x, int pos) {
-        int[] newArray = new int[n + 1];
-        for (int i = 0; i < n + 1; i++) {
-            if (i < pos - 1) newArray[i] = arr[i];
-            else if (i == pos - 1) newArray[i] = x;
-            else newArray[i] = arr[i - 1];
+    public static int getMinValue(int[][] numbers) {
+        int minValue = numbers[0][0];
+        for (int[] number : numbers) {
+            for (int j : number) {
+                if (j < minValue) {
+                    minValue = j;
+                }
+            }
         }
-        return newArray;
+        return minValue ;
     }
+
 
     private static void addValueInArray() {
-        int n = 10;
         int x = 50;
+        int[][] arr = {
+                {13, 43243, 8, 321321312, 43423, -1},
+                {0, 443, 8, 54551, 43423, -10}
+        };
 
-        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        System.out.println("\n------------------------");
-        System.out.println("\nInitial Array:\n" + Arrays.toString(arr));
-
-        arr = addElement(n, arr, x, 5);
-        System.out.println("\nArray with " + x + " added:\n" + Arrays.toString(arr));
-        System.out.println("\n------------------------");
+        System.out.print("\n------------------------");
+        System.out.print("\nInitial Array:\n" + Arrays.deepToString(arr));
+        for(int i = 0 ; i < arr.length ; i++){
+            for(int j = 0 ; j < arr[0].length; j++)
+            {
+                arr[1][2] = x;
+            }
+        }
+        System.out.print("\nArray with " + x + " added:\n" + Arrays.deepToString(arr));
+        System.out.print("\n------------------------");
     }
 
     private static void findDuplicateValuesOfArray() {
@@ -155,9 +178,9 @@ public class Task7 {
                 {5, 1, 2, 3, 4, 2, 7, 8, 8, 3, 5},
                 {5, 1, 2, 3, 5, 2, 7, 8, 7, 3, 2}
         };
-        System.out.println("\n------------------------");
-        System.out.println("\nDuplicate elements in the given array: ");
-
+        System.out.print("\n------------------------");
+        System.out.print("\nDuplicate elements in the given array: ");
+        System.out.print("\n"+Arrays.deepToString(arr));
         Set<Integer> uniqueNumbers = new HashSet<>();
         Set<Integer> duplicateNumbers = Arrays.stream(arr)
                 .flatMapToInt(IntStream::of)
@@ -165,7 +188,7 @@ public class Task7 {
                 .boxed()
                 .collect(Collectors.toSet());
 
-        System.out.println("Duplicate numbers are : " +duplicateNumbers);
+        System.out.print("\nDuplicate numbers are : \n" +duplicateNumbers);
     }
 
     static void bubbleSort(int[][] ints)
@@ -183,7 +206,7 @@ public class Task7 {
         }
 
         for (int[] anInt : ints) {
-            for (int i : anInt) System.out.print(i + " ");
+            for (int i : anInt) System.out.print(i+" ");
         }
     }
 
@@ -193,9 +216,9 @@ public class Task7 {
                     { 9, 5, 3, 2 }
             };
 
-        System.out.println("Unsorted Array ");
-        System.out.println(Arrays.deepToString(unsortedArray));
-        System.out.println("Sorted Array in Ascending Order wit Bubble sort algorithm:");
+        System.out.print("\nUnsorted Array:");
+        System.out.print("\n"+Arrays.deepToString(unsortedArray));
+        System.out.print("\nSorted Array in Ascending Order with Bubble sort algorithm: \n");
         bubbleSort(unsortedArray);
         System.out.println("\n------------------------");
     }
