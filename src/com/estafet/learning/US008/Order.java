d9c20dd275616b5b4089d328726e9d5e2deeb74e
Ivaylo Goranovski <ivaylo.goranovski@gmail.com>
2022-03-15 13:34:18 +0200

    some extends

--- src/com/estafet/learning/US008/Order.java
+++ src/com/estafet/learning/US008/Order.java
@@ -1,4 +1,16 @@
 package com.estafet.learning.US008;
 
 public class Order {
+    public String OrderNumber;
+    public String ClientDetails;
+    public String ListWithArticles;
+    public String TotalAmount;
+}
+
+class TradeOrder extends Order {
+
+}
+
+class TradeInvoice extends Order {
+
 }
