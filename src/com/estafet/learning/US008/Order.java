3dd3344eca0a5833a0273c81c700562473a133ee
Ivaylo Goranovski <ivaylo.goranovski@gmail.com>
2022-04-18 16:00:04 +0300

    change letters

--- src/com/estafet/learning/US008/Order.java
+++ src/com/estafet/learning/US008/Order.java
@@ -3,25 +3,25 @@ package com.estafet.learning.US008;
 public abstract class Order {
     public static final String ORDER_NAME = "ORR";
     public String orderNumber;
-    public String ClientDetails;
-    public String ListWithArticles;
-    public String TotalAmount;
-    private static final int invoice = 1;
+    public String clientDetails;
+    public String listWithArticles;
+    public String totalAmount;
+    private static final int Invoice = 1;
     public static final String color = "green";
 
     public Order(){
     }
 
-    public Order(String orderNumber, String ClientDetails){
+    public Order(String orderNumber, String clientDetails){
         this.orderNumber = orderNumber;
-        this.ClientDetails = ClientDetails;
+        this.clientDetails = clientDetails;
     }
 
-    public Order(String orderNumber,String ClientDetails,String ListWithArticles,String TotalAmount){
+    public Order(String orderNumber,String clientDetails,String listWithArticles,String totalAmount){
         this.orderNumber = orderNumber;
-        this.ClientDetails = ClientDetails;
-        this.ListWithArticles = ListWithArticles;
-        this.TotalAmount = TotalAmount;
+        this.clientDetails = clientDetails;
+        this.listWithArticles = listWithArticles;
+        this.totalAmount = totalAmount;
     }
 }
 
